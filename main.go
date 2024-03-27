572d0de9301566926a7a60d0db0fc6b70249b0a4
Elias Abacioglu <1148206+Raboo@users.noreply.github.com>
2024-03-27 08:35:31 +0100

    update code

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
     // Create a channel to receive OS signals
     signals := make(chan os.Signal, 1)
-    signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, os.Interrupt, os.Kill)
+    signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
     // Print a log message indicating that the pause container has started
     log.Println("Pause container started.")
